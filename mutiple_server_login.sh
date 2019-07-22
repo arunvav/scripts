e96cb29efc0343c6e2ee78dfb3960f5643a91e3e
Arunvignesh <arun06ac104@gmail.com>
2019-07-21 20:59:11 -0400

    Update mutiple_server_login.sh

--- mutiple_server_login.sh
+++ mutiple_server_login.sh
@@ -1,12 +1,12 @@
 for HOST in $(cat servers); 
 do 
 
-sshpass -p "BHHasd99" ssh -o StrictHostKeyChecking=no sysadmin@$HOST "sudo mkdir /script/; sudo chmod 777 /script/; sudo chmod +t /script/";
+sshpass -p "XXXX" ssh -o StrictHostKeyChecking=no sysadmin@$HOST "sudo mkdir /script/; sudo chmod 777 /script/; sudo chmod +t /script/";
 
-sshpass -p "BHHasd99" scp -o StrictHostKeyChecking=no inventory.sh sysadmin@$HOST:/script/;
+sshpass -p "XXX" scp -o StrictHostKeyChecking=no inventory.sh sysadmin@$HOST:/script/;
 
-sshpass -p "BHHasd99" ssh -o StrictHostKeyChecking=no sysadmin@$HOST "sudo sh /script/inventory.sh >> /script/output-$HOST;";
+sshpass -p "XXX" ssh -o StrictHostKeyChecking=no sysadmin@$HOST "sudo sh /script/inventory.sh >> /script/output-$HOST;";
 
-sshpass -p "BHHasd99" scp -o StrictHostKeyChecking=no sysadmin@$HOST:/script/output-* /clients/
+sshpass -p "XXX" scp -o StrictHostKeyChecking=no sysadmin@$HOST:/script/output-* /clients/
 
 done
